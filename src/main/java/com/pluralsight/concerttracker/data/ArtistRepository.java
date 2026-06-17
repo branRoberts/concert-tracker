5100267964c198d85a4b05ec26e60bd3120ce296
branRoberts <brandonkrob12@gmail.com>
2026-06-17 08:42:06 -0400

    Seventh Commit I have added the repositories for Venue, Artist, Promoter, and Concert

---
+++ src/main/java/com/pluralsight/concerttracker/data/ArtistRepository.java
@@ -0,0 +1,7 @@
+package com.pluralsight.concerttracker.data;
+
+import com.pluralsight.concerttracker.models.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistRepository extends JpaRepository<Artist, Integer> {
+}
